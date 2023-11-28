fe0b13176616a975913a255685adf8425bde9933
sasakireika <bknb1743rmsr@gmail.com>
2023-11-28 09:57:46 +0900

    Lesson01実装完了

---
+++ src/main/java/com/example/controller/CourseController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.entity.Course;
+import com.example.service.CourseService;
+
+@Controller
+@RequestMapping("/course")
+public class CourseController {
+
+	private final CourseService courceService;
+	
+	@Autowired
+	public CourseController(CourseService courseService) {
+		this.courceService = courseService;
+	}
+	
+	@GetMapping("/list")
+	public String index(Model model) {
+		List<Course> courses = this.courceService.findAll();
+		model.addAttribute("courses",courses);
+		return "index";
+	}
+}
---
+++ src/main/java/com/example/mapper/CourseMapper.java
@@ -0,0 +1,13 @@
+package com.example.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.entity.Course;
+
+@Mapper
+public interface CourseMapper {
+
+	public List<Course> findAll();
+}
