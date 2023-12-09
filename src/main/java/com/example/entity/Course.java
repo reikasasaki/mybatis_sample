37a8a0ec3518eeba8756a7702c520de6593e8d42
sasakireika <bknb1743rmsr@gmail.com>
2023-12-09 21:29:46 +0900

    lesson03実装完了

--- src/main/java/com/example/entity/Course.java
+++ src/main/java/com/example/entity/Course.java
@@ -1,9 +1,13 @@
 package com.example.entity;
 
+import java.util.List;
+
 public class Course {
 
 	private int id;
 	private String name;
+	private CourseDetail courseDetail;
+	private List<Chapter> chapters;
 	
 	public int getId() {
 		return id;
@@ -17,6 +21,19 @@ public class Course {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public CourseDetail getCourseDetail() {
+		return courseDetail;
+	}
+	public void setCourseDetail(CourseDetail courseDetail) {
+		this.courseDetail = courseDetail;
+	}
+	public List<Chapter> getChapters() {
+		return chapters;
+	}
+	public void setChapters(List<Chapter> chapters) {
+		this.chapters = chapters;
+	}
+	
 	
 	
 }
